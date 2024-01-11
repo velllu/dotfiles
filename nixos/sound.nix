d0ea32ef6655a20b60ff4b8cbb6772b39867cb78
velllu <91963404+velllu@users.noreply.github.com>
2024-01-11 21:21:01 +0100

    Switched from pulseaudio to pipewirew

--- nixos/sound.nix
+++ nixos/sound.nix
@@ -1,6 +1,13 @@
 {
   config = {
-    sound.enable = true;
-    hardware.pulseaudio.enable = true;
+    hardware.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
   };
 }
