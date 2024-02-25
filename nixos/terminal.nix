96330cac7cc00d3e9940ab53d2c378403f4ad6eb
velllu <91963404+velllu@users.noreply.github.com>
2024-02-25 14:00:52 +0100

    Made fish shell follow nix-colors theme

--- nixos/terminal.nix
+++ nixos/terminal.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 
 {
   config = {
@@ -26,21 +26,20 @@
         set -U fish_greeting # disable greeting
         fish_vi_key_bindings
 
-        set -l foreground c0caf5
-        set -l selection 283457
-        set -l comment 565f89
-        set -l red f7768e
-        set -l orange ff9e64
-        set -l yellow e0af68
-        set -l green 9ece6a
-        set -l purple 9d7cd8
-        set -l cyan 7dcfff
-        set -l pink bb9af7
+        set -l foreground ${config.vellu.theming.colorScheme.colors.base07}
+        set -l selection ${config.vellu.theming.colorScheme.colors.base01}
+        set -l comment ${config.vellu.theming.colorScheme.colors.base00}
+        set -l red ${config.vellu.theming.colorScheme.colors.base08}
+        set -l orange ${config.vellu.theming.colorScheme.colors.base09}
+        set -l yellow ${config.vellu.theming.colorScheme.colors.base0A}
+        set -l green ${config.vellu.theming.colorScheme.colors.base0B}
+        set -l purple ${config.vellu.theming.colorScheme.colors.base0E}
+        set -l cyan ${config.vellu.theming.colorScheme.colors.base0C}
 
         # Syntax Highlighting Colors
         set -g fish_color_normal $foreground
         set -g fish_color_command $cyan
-        set -g fish_color_keyword $pink
+        set -g fish_color_keyword $cyan
         set -g fish_color_quote $yellow
         set -g fish_color_redirection $foreground
         set -g fish_color_end $orange
@@ -50,7 +49,7 @@
         set -g fish_color_selection --background=$selection
         set -g fish_color_search_match --background=$selection
         set -g fish_color_operator $green
-        set -g fish_color_escape $pink
+        set -g fish_color_escape $orange
         set -g fish_color_autosuggestion $comment
 
         # Completion Pager Colors
@@ -63,7 +62,7 @@
 
       shellAliases = {
         cat = "bat --style plain -P";
-        clear = "clear && pfetch";
+        clear = "clear && fastfetch -l nixos_small";
         grep = "rg";
         less = "moar";
         ls = "eza --long --icons --git --no-permissions --sort type";
