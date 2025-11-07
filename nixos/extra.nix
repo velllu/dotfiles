7dfefb6070f1ae526409b8aec86b65d8191e740f
velllu <a>
2025-11-07 22:27:15 +0100

    Added sunshine game streaming

---
+++ nixos/extra.nix
@@ -0,0 +1,24 @@
+{ pkgs, ... }:
+
+{
+  config = {
+    environment.systemPackages = with pkgs; [
+      blender
+      gimp
+      godot_4
+      heroic
+      jetbrains.idea-community
+      kdePackages.kdenlive
+      mangohud
+      prismlauncher
+      reaper
+      tor-browser-bundle-bin
+    ];
+
+    # Game streaming
+    services.sunshine = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
+}
