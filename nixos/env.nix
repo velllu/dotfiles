a3236eba346a7bcc2532c711923fc92683d35fe8
velllu <a>
2025-12-21 14:25:45 +0100

    Added compose key

--- nixos/env.nix
+++ nixos/env.nix
@@ -1,10 +1,12 @@
 {
   config = {
     environment.sessionVariables = {
-      GIT_ASKPASS = "";
       EDITOR = "code";
       EZA_ICON_SPACING = "2";
+      GIT_ASKPASS = "";
+      
+      # Remaps the caps lock key to the compose key to be able to type accented letters
+      XKB_DEFAULT_OPTIONS = "compose:caps";
     };
   };
 }
-
